da5c71ed636dd7b5c395e2d0256e9945937b0137
Daniele <pucciodaniele95@gmail.com>
2022-05-18 16:41:49 +0200

    undefined card

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, R
 Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.*/
 
 const myTeamContainer = document.querySelector(".team-container");
-const myTeamCard = document.querySelector(".team-card");
-const myCardImg = document.querySelector(".card-image");
-const myCardText = document.querySelector(".card-text");
+// const myTeamCard = document.querySelector(".team-card");
+// const myCardImg = document.querySelector(".card-image");
+// const myCardText = document.querySelector(".card-text");
 
+// membri del team
 const membersTeam = 
 [
     {
@@ -49,3 +50,40 @@ const membersTeam =
 ];
 
 console.log(membersTeam);
+
+// Funzione per creare elementi nel DOM
+const createMyEl = (myClass, myEl) => {
+    const myElement = document.createElement(myEl);
+    myElement.className = myClass;
+    return myElement;
+}
+
+for (let i = 0; i < membersTeam.length; i++){
+    let cardMembers = membersTeam[i];
+
+
+// Aggiungo i vari elementi nel DOM
+// let myContainerEl = createMyEl("team-container");
+
+let myCard = createMyEl("team-card", "div");
+
+let myCardImage = createMyEl("card-image", "div");
+
+let myImg = document.createElement("img", "div");
+
+let myText = createMyEl("card-text", "div");
+
+myImg.src = cardMembers.image;
+
+myText.innerHTML = `<h3>${cardMembers.nome}</h3><p>${cardMembers.ruolo}</p>`;
+
+myCardImage.append(myImg);
+
+myCard.append(myCardImage);
+
+myCard.append(myText);
+
+myTeamContainer.append(myCard);
+
+};
+
