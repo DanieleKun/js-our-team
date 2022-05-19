95a79dfe63ea05f926a6fcdd61fc3cf90d874e05
Daniele <pucciodaniele95@gmail.com>
2022-05-19 09:32:17 +0200

    clean code

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-/*Viene fornito un layout di base in cui è presente una card di esempio inserita staticamente nell’html.
-Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e dovrà quindi essere rimossa dall’html.
-Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team.
-Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
-Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.*/
-
 const myTeamContainer = document.querySelector(".team-container");
-// const myTeamCard = document.querySelector(".team-card");
-// const myCardImg = document.querySelector(".card-image");
-// const myCardText = document.querySelector(".card-text");
 
 // membri del team
 const membersTeam =
@@ -27,13 +18,13 @@ const membersTeam =
         {
             "name": "Walter Gordon",
             "role": "Office Manager",
-            "image": "img/angela-lopez-social-media-manager.jpg"
+            "image": "img/walter-gordon-office-manager.jpg"
         },
 
         {
             "name": "Angela Lopez",
             "role": "Social Media Manager",
-            "image": "img/angela-caroll-chief-editor.jpg"
+            "image": "img/angela-lopez-social-media-manager.jpg"
         },
 
         {
@@ -81,6 +72,7 @@ for (let i = 0; i < membersTeam.length; i++) {
     myCard.append(myText);
 
     myTeamContainer.append(myCard);
-
 };
+
+
 
